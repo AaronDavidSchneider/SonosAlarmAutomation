0c1d112b24ba44875e55ae96a2da9286335bc543
Aaron <Aaron.David.Schneider@gmail.com>
2020-01-17 12:08:56 +0100

    inital version

---
+++ apps/SonosAlarm/SonosAlarm.py
@@ -0,0 +1,79 @@
+import appdaemon.plugins.hass.hassapi as hass
+
+class SonosAlarm(hass.Hass):
+    def initialize(self):
+        entities = [
+            "group.sonos_alarm",
+            "input_number.alarm_volume",
+            "input_number.alarm_snooze",
+            "input_number.alarm_clock_hour",
+            "input_number.alarm_clock_minute",
+        ]
+        self.target = None
+
+        for e in entities:
+            self.listen_state(self.update_alarm, e)
+
+    def update_alarm(self, entity=None, attribute=None, old=None, new=None, kwargs=None):
+        self.log("update_alarm")
+        alarm_correct = self.check_alarm()
+        if not alarm_correct:
+            ids = self.args["ids"]
+            hour = float(self.get_state("input_number.alarm_clock_hour"))
+            minute = float(self.get_state("input_number.alarm_clock_minute"))
+            snooze = float(self.get_state("input_number.snooze"))
+            volume = float(self.get_state("input_number.alarm_volume"))
+            enabled = self.get_state("group.sonos_alarm")
+            times = []
+
+
+            for i in range(len(ids)):
+                m = (minute + i * snooze) % 60
+                h = hour + int((minute + i * snooze) / 60)
+                time = "{:02d}:{:02d}:00".format(int(h) , int(m))
+                times.append(time)
+
+                self.call_service(
+                    "sonos/update_alarm",
+                    entity_id=self.args["entity_id"],
+                    alarm_id=ids[i],
+                    time=time,
+                    volume=volume,
+                    enabled=enabled,
+                )
+
+                self.call_service(
+                    "homeassistant/update_entity",
+                    entity_id="switch.sonos_alarm_{}".format(ids[i]),
+                )
+
+            self.target = {
+                "ids": ids,
+                "times": times,
+                "enabled": enabled,
+                "volume": volume,
+            }
+            self.log("Next iteration")
+            self.run_in(self.update_alarm, 2)
+        else:
+            self.log("Wecker updated correctly")
+            self.target=None
+
+    def check_alarm(self):
+        if self.target is None:
+            return False
+        else:
+            for i in range(len(self.target["ids"])):
+                state = self.get_state(
+                    "switch.sonos_alarm_{}".format(self.target["ids"][i]), attribute="all"
+                )
+                attr = state["attributes"]
+                state = state["state"]
+
+                if (
+                    state != self.target["enabled"]
+                    or attr["time"] != self.target["times"][i]
+                    or attr["volume"] != self.target["volume"]
+                ):
+                    return False
+            return True
--- info.md
+++ info.md
@@ -1,14 +1,53 @@
+## Requirements
+`SonosAlarm` - https://github.com/AaronDavidSchneider/SonosAlarm
+
 ## App configuration
 
 ```yaml
-hacs:
-  module: hacs
-  class: Hacs
-  hacs_sensor: sensor.hacs
+alarm:
+  module:    SonosAlarm
+  class:     SonosAlarm
+  entity_id: [your media_player]
+  ids:
+    - [ your alarm ids ]
+```
+## Additional configuration needed:
+
+`groups.yaml`
+```yaml
+sonos_alarm:
+    name: SONOS ALARM
+    icon: mdi:alarm
+    entities:
+      - switch.sonos_alarm_[ your alarm ids ]
 ```
 
-key | optional | type | default | description
--- | -- | -- | -- | --
-`module` | False | string | | The module name of the app.
-`class` | False | string | | The name of the Class.
-`hacs_sensor` | True | string | `sensor.hacs`| The entity_id of the HACS sensor.
+`configuration.yaml`
+```yaml
+input_number:
+    alarm_snooze:
+      name: Snooze Dauer
+      min: 5 #optional
+      max: 60 #optional
+      step: 5 #optional
+      icon: mdi:sleep #optional
+    alarm_clock_hour:
+      name: Stunde
+      icon: mdi:timer #optional
+      min: 4 #optional
+      max: 10 #optional
+      step: 1 #optional
+    alarm_clock_minute:
+      name: Minute
+      icon: mdi:timer #optional
+      min: 0 #optional
+      max: 55 #optional
+      step: 5 #optional
+    alarm_volume:
+      name: Lautstärke Wecker
+      icon: mdi:volume-high #optional
+      min: 0.0 #optional
+      max: 0.3 #optional
+      mode: box #optional
+```
+`min`, `max`,`step`,`mode`,`icon` are just personal preferences and optional.
